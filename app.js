6936a2a220099fa1af691b85e61a955f163421a7
Junjie Ge <jg5324@nyu.edu>
2019-11-05 15:07:47 -0500

    initial commit, hello world

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+var app = express();
+const hostname = "127.0.0.1";
+const port = 8000;
+
+const indexRoute = require('./routes/index.js');
+
+app.use('/', indexRoute);
+
+app.use(express.static('public'));
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!");
+})
+
+// Prints a log once the server starts listening
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
