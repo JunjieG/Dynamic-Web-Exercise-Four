02ab231316aa1cfe63880b90361f387757e273fe
Junjie Ge <jg5324@nyu.edu>
2019-11-07 16:16:05 -0500

    added firebase sdk, and connected to database

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Initialize Express.js
 const express = require('express');
 
 var app = express();
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,65 @@
 const express = require('express');
 const router = express.Router();
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC9tsUDt_k22FXcTRSETIYSR2uJoBOUC20",
+  authDomain: "idm-exercise-four.firebaseapp.com",
+  databaseURL: "https://idm-exercise-four.firebaseio.com",
+  projectId: "idm-exercise-four",
+  storageBucket: "idm-exercise-four.appspot.com",
+  messagingSenderId: "286645803715",
+  appId: "1:286645803715:web:6d93d7baf9166ed5cca076"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+
+// Getting all articles
 router.get('/', function (req, res) {
-  res.send('hello world!!!!');
+  var blogs = []
+  let blogRef = db.collection('blog-posts');
+  let allBlogs = blogRef.get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        blogs.push({
+          id: doc.id,
+          content: doc.data()
+        })
+      });
+      res.send(blogs)
+    })
+    .catch(err => {
+      console.log('Error', err)
+      res.send('Article not found...');
+    });
+})
+
+// Getting specific articles
+router.get('/:id', function (req, res) {
+  let blogRef = db.collection('blog-posts').doc(req.params.id);
+  let getDoc = blogRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+        res.send('No such document!');
+      } else {
+        console.log('Document data:', doc.data());
+        res.send(doc.data());
+      }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+      res.send('Error getting document');
+    });
 })
 
 module.exports = router;
